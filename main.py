98f708d40f19a62c08d0cc120dd29d0da4697b82
Jan Zehnder <J.Zehnder.inf24@stud.bbbaden.ch>
2026-09-07 11:02:53 +0200

    Connect application to SQLite and PostgreSQL #1

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, Response, redirect, render_template, request, url_for
+from database import db
+
 import helper
-from flask import Flask, request, render_template, redirect, url_for
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}".format(
+    dbuser=os.environ["DBUSER"],
+    dbpass=os.environ["DBPASS"],
+    dbhost=os.environ["DBHOST"],
+    dbname=os.environ["DBNAME"],
+)
+
+db.init_app(app)
+app.app_context().push()
+db.create_all()
+
+
+@app.route("/getCSV")
+def get_csv():
+    return Response(
+        helper.get_csv(),
+        mimetype="text/csv",
+        headers={"Content-disposition": "attachment; filename=zu-bbbearbeiten.csv"},
+    )
 
 
 @app.route("/")
 def index():
-    items = helper.get_all()
+    items = helper.get_all(sorted=True)
     return render_template("index.html", items=items)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    title = request.form.get("title")
-    date = request.form.get("date")
-    helper.add(title, date)
+    text = request.form.get("text")
+    date = request.form.get("deadline")
+    category = request.form.get("category")
+    description = request.form.get("description")
+
+    helper.add(
+        text,
+        date=date,
+        category=category,
+        description=description,
+    )
+
     return redirect(url_for("index"))
 
 
